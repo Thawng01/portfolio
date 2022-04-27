ff5405cd71983c3f88f221b81817ec1b7483b80c
thechin press <thechinpress@gmail.com>
2022-04-27 11:32:45 +0630

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,20 @@ import "./home.css";
 
 const Home = ({ onContact }) => {
     const ref = useRef();
-    const itemRef = useRef();
 
     useEffect(() => {
         setTimeout(() => {
-            itemRef?.current?.classList.add("home-animation");
+            ref?.current?.classList.add("home-animation");
         }, 100);
     }, []);
 
     return (
         <div ref={ref} className="home">
-            <div ref={itemRef} className="home-info-container">
-                <h1 className="home-name">Looking for web developer</h1>
-                <p className="home-goal">
-                    My aim is to satisfy you with my skills
-                </p>
-                <button className="home-contact-btn" onClick={onContact}>
-                    Contact me
-                </button>
-            </div>
+            <h1 className="home-name">Looking for web developer</h1>
+            <p className="home-goal">My aim is to satisfy you with my skills</p>
+            <button className="home-contact-btn" onClick={onContact}>
+                Contact me
+            </button>
         </div>
     );
 };
